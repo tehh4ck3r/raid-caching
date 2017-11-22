22bc191e976a6c751f030502751ed810421ab889
Kush Mahajani <kmahajani@scu.edu>
2017-11-21 21:59:36 -0700

    Commenting

--- second-chance.py
+++ second-chance.py
@@ -22,48 +22,58 @@ def main(argv):
 	lines = map(int, lines) # make everything into an int
 
 	num_misses = 0
-	max_val = max(lines)
+	max_val = max(lines) # get max value for binning
 
-	clock = []
+	cachelist = [] # list containing each device's cache
+	clock = [] # each device has an independent clock
 
-	cachelist = []
 	max_indiv_cache_size = max_pages // num_devices # max_indiv_cache_size = max cache size per device
-	for x in range(num_devices):
+	
+	# initialize the per-device caches with empty pages
+	for x in range(num_devices):		
+		# initialize a new device with empty/invalid pages
 		cache = []
 		for y in range(max_indiv_cache_size):
 			cache.append(page(-1, False))
-		cachelist.append(cache)
-		clock.append(0)
 
+		cachelist.append(cache) # add the new device to the list
+
+		clock.append(0) # set the device's cache clock to 0
+
+	# for each line
 	for x in lines:
 		bin_num = bins(max_val, num_devices, x) # get the device that block x would be stored on
 
 		found = False 
 
+		# find out if the value is in the cache
 		for y in range(max_indiv_cache_size):
-			if cachelist[bin_num][y].value == x:
-				cachelist[bin_num][y].referenced = True
+			if cachelist[bin_num][y].value == x: # we found it!
+				cachelist[bin_num][y].referenced = True # set its referenced flag to True
 				found = True
 				break
 
-		if not found:
-			i = clock[bin_num]
+		if not found: # it was a miss
+			i = clock[bin_num] # used for incrementing
 			while True:
-				if not cachelist[bin_num][i].referenced:
+				if not cachelist[bin_num][i].referenced: # find the next unreferenced element
 					clock[bin_num] = i
 					break
 
-				cachelist[bin_num][i].referenced = False
+				cachelist[bin_num][i].referenced = False # set element's referenced flag to False
 
-				if i == clock[bin_num] - 1:
+				if i == clock[bin_num] - 1: # stop the loop if we made it all the way around the cache
 					break
 
+				# traverse the cache circularly
 				i += 1
 				i %= max_indiv_cache_size
 
-
-			cachelist[bin_num][clock[bin_num]].value = x
-			cachelist[bin_num][clock[bin_num]].referenced = True
+			# we found our unreferenced element, so replace it with the new value
+			cachelist[bin_num][clock[bin_num]].value = x # replace element clock hand is pointing at
+			cachelist[bin_num][clock[bin_num]].referenced = True # set its reference flag to True
+			
+			# advance the clock by 1, circularly
 			clock[bin_num] += 1
 			clock[bin_num] %= max_indiv_cache_size
 
